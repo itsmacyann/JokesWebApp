32985d0e255f1e48065657dc392b34736651d754
itsmacyann <macyann.austria@gmail.com>
2023-05-19 13:08:53 +0800

    Add project files.

---
+++ Models/Joke.cs
@@ -0,0 +1,10 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+    }
+}
